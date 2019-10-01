be88662cb8fb973f5198e12062a5c2b17dc479df
Julia <iulia.golicova@gmail.com>
2019-09-30 21:43:42 -0700

    Finished Lab 1

--- PasswordFile.h
+++ PasswordFile.h
@@ -8,46 +8,46 @@ using namespace std;
 class PasswordFile
 {
     public: 
-        PasswordFile(string filename);                     // opens the file and reads the names/passwords in the vectors user and password.
+        PasswordFile(string filename);                      // opens the file and reads the names/passwords in the vectors user and password.
         void addpw (string newuser, string newpassword);    //this adds a new user/password to the vectors and writes the vectors to the file filename
-        void deleteUser (string oldUser);                     // deletes existing user and password
+        void deleteUser (string oldUser);                   // deletes existing user and password
         bool checkpw (string user, string passwd);          // returns true if user exists and password matches
+        static void newsalt(int ns);
         
     private: 
+        bool encryptPasswords;                             // Turn true to encrypt string onto filename
+        static int salt;
         string filename;                                    // the file that contains password information
         vector <string> user;                               // the list of usernames
         vector <string> password;                           // the list of passwords
         void synch();                                       // writes the user/password vectors to the password file
+        string encrypt(string s);
 };
 
 PasswordFile::PasswordFile(string filename)
-{
-    ifstream infile; 
-    
-    // Check if inFile exist, if not then create file with filename
-    if (!infile.)
-    {
-        
-    }
-    
-    infile.open(filename.c_str);
-    
+{   
+    this->filename = filename;
     string u, p; 
-    infile >> u >> p; 
-    while (infile.good())
+    ifstream passwordFile;
+    passwordFile.open(this->filename.c_str());
+    while (passwordFile.good())
     {
         user.push_back(u);
         password.push_back(p);
-        infile >> u >> p; 
+        passwordFile >> u >> p; 
     }
-    infile.close();   
 }
 
 void PasswordFile::addpw (string newuser, string newpassword)
 {
+    encryptPasswords = true;
+    
+    // Declaring iterator to a vector 
+    vector<string>::iterator it = user.begin(); 
+    it = find(user.begin(), user.end(), newuser);
+
     // Check if user already exist, if so update only password
-    iterator (user.begin(), user.end(), newuser)
-    if(iterator == user.end()) 
+    if(it == user.end()) 
     {
         user.push_back(newuser);            // Add newuser
         password.push_back(newpassword);    // Add newpassword
@@ -57,13 +57,12 @@ void PasswordFile::addpw (string newuser, string newpassword)
     {
         cout << "User already exists." << endl; 
     }    
-
 }
 
 bool PasswordFile::checkpw (string user, string passwd)
 {
     // Iterate through each user (should be parallel with password vector)
-    for (int i = 0; i < PasswordFile::user.size; i++)
+    for (int i = 0; i < PasswordFile::user.size(); i++)
     {
         if (PasswordFile::user[i] == user)
         {
@@ -80,16 +79,20 @@ bool PasswordFile::checkpw (string user, string passwd)
 void PasswordFile::synch()
 {
     ofstream outFile;
-    outfile.open(filename.c_str());
+    outFile.open(filename.c_str());
     for(int i = 0; i < user.size(); i++)
     {
-        outFile << user[i] << " " << password[i] << endl;
+        if (encryptPasswords)
+            outFile << user[i] << " " << encrypt(password[i]) << endl;
+        else
+            outFile << user[i] << " " << password[i] << endl;
     }
     outFile.close();
 }
 
 void PasswordFile::deleteUser (string oldUser)
 {
+    int i = 0; 
     while (i < user.size())
     {
         if(user[i] == oldUser)
@@ -101,11 +104,35 @@ void PasswordFile::deleteUser (string oldUser)
             // Delete the last element of the vectors
             user.pop_back(); 
             password.pop_back();
-
+            
+            break;
         }
         else 
-            i++;
-            
+        {
+             i++;
+        }
     }
-    synch()
+
+    if (i >= user.size())
+        cout << "Could not find " << oldUser << " in " << filename << endl;
+    
+    synch();
+}
+
+// encryption 
+
+int PasswordFile::salt = 1;
+
+ void PasswordFile::newsalt(int ns)
+ { 
+    salt = ns;
+ }
+  
+string PasswordFile::encrypt(string s)
+{ 
+    for (int i=0; i<s.size(); i++)
+    { 
+      s[i]+=salt;
+    }
+    return s;
 }
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <iostream> 
 #include "PasswordFile.h"
 
@@ -6,9 +7,37 @@ using namespace std;
 int main() 
 {
     PasswordFile passfile("password.txt");
+
+    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    cout << endl; 
+
     passfile.addpw("dbotting","123qwe");
     passfile.addpw("egomez","qwerty");
     passfile.addpw("tongyu","liberty");
-    
-    //check if passwords match users
+
+    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    cout << endl;
+
+    // right password 
+    cout << "Password for egomez : " << passfile.checkpw("egomez", "qwerty");
+    cout << endl;
+    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    cout << endl; 
+
+    // wrong password 
+    cout << "Password for egomez : " << passfile.checkpw("egomez", "qwertyyy"); 
+    cout << endl;
+
+    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    cout << endl; 
+
+    passfile.deleteUser("egomez");
+
+    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    cout << endl; 
+
+    passfile.deleteUser("tong");
+
+    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    cout << endl; 
 }
