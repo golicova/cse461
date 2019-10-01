de5cc215921ee30dfcc5e865b70ccfdbe2beeddd
Ryan Zagala <gman10501@gmail.com>
2019-10-01 10:41:28 -0700

    Organized Labs

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -8,36 +8,36 @@ int main()
 {
     PasswordFile passfile("password.txt");
 
-    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    //system( "read -n 1 -s -p \"Press any key to continue...\"" );
     cout << endl; 
 
     passfile.addpw("dbotting","123qwe");
     passfile.addpw("egomez","qwerty");
     passfile.addpw("tongyu","liberty");
 
-    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    //system( "read -n 1 -s -p \"Press any key to continue...\"" );
     cout << endl;
 
     // right password 
     cout << "Password for egomez : " << passfile.checkpw("egomez", "qwerty");
     cout << endl;
-    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    //system( "read -n 1 -s -p \"Press any key to continue...\"" );
     cout << endl; 
 
     // wrong password 
     cout << "Password for egomez : " << passfile.checkpw("egomez", "qwertyyy"); 
     cout << endl;
 
-    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    //system( "read -n 1 -s -p \"Press any key to continue...\"" );
     cout << endl; 
 
     passfile.deleteUser("egomez");
 
-    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    //system( "read -n 1 -s -p \"Press any key to continue...\"" );
     cout << endl; 
 
     passfile.deleteUser("tong");
 
-    system( "read -n 1 -s -p \"Press any key to continue...\"" );
+    //system( "read -n 1 -s -p \"Press any key to continue...\"" );
     cout << endl; 
 }
