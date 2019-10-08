0d2f4cccc989c99853eb060f5153b02de7a53829
Ryan Zagala <gman10501@gmail.com>
2019-10-08 10:31:15 -0700

    Initial setup for lab 3

--- Lab3/Filesys.h
+++ Lab3/Filesys.h
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <vector>
+#include "Sdisk.h"
 
 class Filesys: public Sdisk
 {
     public :
+        Filesys();
         Filesys(string diskname, int numberofblocks, int blocksize);
         int fsclose();
         int fssynch();
@@ -17,14 +20,68 @@ class Filesys: public Sdisk
         int writeblock(string file, int blocknumber, string buffer);
         int nextblock(string file, int blocknumber);
     private :
-        int rootsize;           // maximum number of entries in ROOT
-        int fatsize;            // number of blocks occupied by FAT
-        vector<string> filename;   // filenames in ROOT
-        vector<int> firstblock; // firstblocks in ROOT
-        vector<int> fat;             // FAT
+        int rootsize;                   // maximum number of entries in ROOT
+        int fatsize;                    // number of blocks occupied by FAT
+        vector<string> filename;        // filenames in ROOT
+        vector<int> firstblock;         // firstblocks in ROOT
+        vector<int> fat;                // FAT
 };
 
-int::Filesys fsclose()
+Filesys::Filesys() 
+{
+    
+}
+
+Filesys::Filesys(string diskname, int numberofblock, int blocksize)
+{
+
+}
+
+int Filesys::fsclose()
+{
+
+}
+
+int Filesys::fssynch()
+{
+
+}
+
+int Filesys::newfile(string file)
+{
+
+}
+
+int Filesys::rmfile(string file)
+{
+    
+}
+int Filesys::getfirstblock(string file)
+{
+    
+}
+
+int Filesys::addblock(string file, string block)
+{
+
+}
+
+int Filesys::delblock(string file, int blocknumber)
+{
+    
+}
+
+int Filesys::readblock(string file, int blocknumber, string& buffer)
+{
+
+}
+
+int Filesys::writeblock(string file, int blocknumber, string buffer)
+{
+    
+}
+
+int Filesys::nextblock(string file, int blocknumber)
 {
     
 }
