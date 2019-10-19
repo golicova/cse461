792a16104cee9f04959acfd04c9b9e55f5be23cf
Julia <iulia.golicova@gmail.com>
2019-10-19 12:01:46 -0700

    Lab 3

--- Lab3/Filesys.h
+++ Lab3/Filesys.h
@@ -27,18 +27,18 @@ class Filesys: public Sdisk
         vector<int> fat;                // FAT
 };
 
-Filesys::Filesys() { }
+Filesys::Filesys() {}
 
 Filesys::Filesys(string diskname, int numberofblock, int blocksize)
 {
-
-    Sdisk sdisk(disk, int numberofblocks, int blocksize);
+    Sdisk sdisk (disk, int numberofblocks, int blocksize);  // How do we utilize this Sdisk object in this constructor?
 
     // Check if Sdisk has a filesystem
     string buffer; 
     getblock (1, buffer); 
 
-    if(buffer[1] == "#")               // empty 
+    // empty 
+    if(buffer[1] == "#") 
     {
         // no file, create and store
         rootsize = getBlockSize() / 13;
@@ -87,7 +87,6 @@ int Filesys::fsclose()
 int Filesys::fssynch()
 {
     // Write Root to the disk
-
     // Write FAT to the disk
 }
 
@@ -97,21 +96,116 @@ int Filesys::newfile(string file)
     // change it to filename 
     // change 0 to 4 
     // change 4 in FAT to 5 (next free block)
+
+    for (int i = 0; i < rootsize; i++)
+    {
+        if (filename[i] == file)
+        {
+            cout << "file name" << endl;
+            return 0;
+        }
+    }
+    for (int i = 0; i < rootsize; i++)
+    {
+        if (filename[i] == "xxxxxxxx")
+        {
+            fssynch();
+            return 1;
+        }
+    }
 }
 
 int Filesys::rmfile(string file)
 {
-    
+    for (int i = 0; i < rootsize, i++)
+    {
+        if(filename[i] ==  file)
+        {
+            if(firstblock[0] != 0)
+            {
+                cout << "File not empty.";
+                return 0; 
+            }
+        }
+        else 
+        {
+            filename[i] = "xxxxxxxx"
+            fssynch(); 
+            return 1; 
+        }
+    }
+    return 0; 
 }
 
 int Filesys::getfirstblock(string file)
 {
-    
+    for (int i = 0; i < rootsize; i++)
+    {
+        if (filename[i] == file)
+        {
+            return firstblock[i];
+        }
+        else 
+        {
+            return -1;
+        }
+    }
+
+    cout << "No such file exists!" << endl;
+    return 0;
 }
 
 int Filesys::addblock(string file, string block)
 {
+    int first = getfirstblock(file);
 
+    if (first == -1)
+    {
+        return 0;
+    }
+
+    int allocate == fat[0];
+    if (allocate == 0)
+    {
+        // no free blocks
+        return 0; 
+    }
+    
+    else 
+    {
+        fat[0] = fat[fat[0];
+        fat[allocate] = 0; 
+    }
+    
+    if (first == 0)
+    {
+        for (int i = 0; i < rootsize; i++)
+        {
+            if (filename[i] == file)
+            {
+                firstblock[i] = allocate;
+                fssynch();
+                putblock(allocate, buffer);     // Where is our buffer value coming from?
+                return allocate;
+            }
+        }
+    }
+    else
+    {
+        cout << "Not Empty" << endl;
+    }
+    
+    int iblock = first;
+    
+    while (fat[iblock] != 0)
+    {
+        iblock = fat[iblock];     // points to first 0 in FAT
+    }
+    fat[iblock] = allocate; 
+    
+    fssynch();
+    putblock(allocate, buffer);     // Where is our buffer value coming from?
+    return allocate;
 }
 
 int Filesys::delblock(string file, int blocknumber)
